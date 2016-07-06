78fed4c62f07bc9ed6a8c16d1ad45f7546a20cf4
Fabricio Valadares <valadares.fabricio@gmail.com>
2016-07-06 17:32:25 -0300

    Update

---
+++ src/Monitor.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Cityware\Monitoring;
+
+/**
+ * Description of Monitor2
+ *
+ * @author fsvxavier
+ */
+class Monitor {
+    //put your code here
+}
