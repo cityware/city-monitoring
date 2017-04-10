ebaf1634d12308603758811e55382f0fecd7a799
Fabricio Valadares <valadares.fabricio@gmail.com>
2017-04-10 12:16:32 -0300

    Update

--- src/Models/Devices/DataDisk.php
+++ src/Models/Devices/DataDisk.php
@@ -7,6 +7,7 @@
  */
 
 namespace Cityware\Monitoring\Models\Devices;
+
 use Cityware\Monitoring\Models\AbstractModels;
 use Exception;
 
@@ -18,6 +19,41 @@ use Exception;
 class DataDisk extends AbstractModels {
 
     public function setDataDisk(array $params, array $paramsDevices) {
+        $this->getConnection();
+        try {
+            foreach ($params['index'] as $index) {
+                if ($params['total_size'][$index] > 0) {
+                    $used_bytes = $params['total_size'][$index] * ($params['used_percent'][$index] / 100);
+
+                    $this->db->sequence('gen_data_disk', 'nocomdata');
+                    $id = $this->db->executeSequence();
+
+                    $paramsDiskInsert = [
+                        'index' => 'nocom',
+                        'type' => 'tab_data_disk',
+                        'id' => $id['0']['nextval'],
+                        'body' => [
+                            "cod_device" => $paramsDevices['cod_device'],
+                            "des_path" => $params['path'][$index],
+                            "num_total_size" => $params['total_size'][$index],
+                            "num_used_percent" => $params['used_percent'][$index],
+                            "num_free_percent" => $params['free_percent'][$index],
+                            "num_total_bytes" => $params['total_size'][$index],
+                            "num_free_bytes" => ($params['total_size'][$index] - $used_bytes),
+                            "num_used_bytes" => $used_bytes,
+                            "dte_register" => date('Y-m-d H:i:s'),
+                        ],
+                    ];
+
+                    $ret = $this->es->index($paramsDiskInsert);
+                }
+            }
+        } catch (Exception $exc) {
+            throw new Exception('Error While Insert Data CPU for JOB PARALLEL - ' . $exc->getMessage());
+        }
+    }
+
+    public function setDataDiskDb(array $params, array $paramsDevices) {
         $this->getConnection();
         try {
             $this->db->transaction();
@@ -45,6 +81,41 @@ class DataDisk extends AbstractModels {
     }
 
     public function setIoDataDisk(array $params, array $paramsDevices) {
+        $this->getConnection();
+        try {
+            foreach ($params['index'] as $index) {
+                if (($params['disk_io_read'][$index] > 0) and ( $params['disk_io_write'][$index] > 0)) {
+
+                    $this->db->sequence('gen_data_disk_io', 'nocomdata');
+                    $id = $this->db->executeSequence();
+
+                    $paramsDiskIoInsert = [
+                        'index' => 'nocom',
+                        'type' => 'tab_data_disk_io',
+                        'id' => $id['0']['nextval'],
+                        'body' => [
+                            "cod_device" => $paramsDevices['cod_device'],
+                            "nam_device_disk" => $params['device'][$index],
+                            "num_disk_io_read" => $params['disk_io_read'][$index],
+                            "num_disk_io_write" => $params['disk_io_write'][$index],
+                            "num_disk_io_read_access" => $params['disk_io_read_access'][$index],
+                            "num_disk_io_write_access" => $params['disk_io_write_access'][$index],
+                            "num_disk_io_load1Min" => $params['disk_io_load1Min'][$index],
+                            "num_disk_io_load5Min" => $params['disk_io_load5Min'][$index],
+                            "num_disk_io_load15Min" => $params['disk_io_load15Min'][$index],
+                            "dte_register" => date('Y-m-d H:i:s'),
+                        ],
+                    ];
+
+                    $ret = $this->es->index($paramsDiskIoInsert);
+                }
+            }
+        } catch (Exception $exc) {
+            throw new Exception('Error While Insert Data CPU for JOB PARALLEL - ' . $exc->getMessage());
+        }
+    }
+
+    public function setIoDataDiskDb(array $params, array $paramsDevices) {
 
         $this->getConnection();
         try {
